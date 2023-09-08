0ef39a75e2660fbcee6fd1c1e51db5951f02f52f
Johan Aspeling <johan.aspeling@gmail.com>
2023-09-08 12:44:16 +0200

    chore: added error-logger lib

--- apps/error-handling/src/app/app.component.ts
+++ apps/error-handling/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { errorLogger } from '@error-handling/error-logger';
 
 @Component({
   selector: 'error-handling-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'error-handling';
+
+  constructor() {
+    errorLogger();
+  }
 }
---
+++ libs/error-logger/src/lib/error-logger.ts
@@ -0,0 +1,4 @@
+export function errorLogger(): string {
+  console.log('[error-logger] init');
+  return 'error-logger';
+}
